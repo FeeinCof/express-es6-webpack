f2751d08bbc26983be6aec8b024ca0008b158b8a
FeeinCof <tuan.letrong.dev@gmail.com>
2023-01-07 07:51:36 +0700

    add vercel.json

--- dist-server/routes/users.js
+++ dist-server/routes/users.js
@@ -7,8 +7,6 @@ exports["default"] = void 0;
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var router = _express["default"].Router();
-
-/* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,6 @@
-// routes/index.js and users.js
 import express from "express";
-var router = express.Router();
-// ..stuff below
+const router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
